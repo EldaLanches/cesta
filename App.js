b99daea70b2fbd6292925b29070899e6f641254b
Eloá Casarotto <eloa.casarotto@escola.pr.gov.br>
2026-03-02 13:05:39 +0000

    aula 04 05

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
-import {View} from 'react-native';
+import { View } from 'react-native';
 import Cesta from './src/Telas/Cesta';
 import { SafeAreaView } from 'react-native-web';
 import {
-        useFonts,
-        Montserrat_400Regular,
-        Montserrat_700Bold
-      } from '@expo-google-fonts/montserrat';
+      useFonts,
+      Montserrat_400Regular,
+      Montserrat_700Bold
+    } from '@expo-google-fonts/montserrat' ;
+
 
 export default function App() {
- const [fontCarregada] = useFonts({
-  "MontserratRegular":Montserrat_400Regular,
+const [fontCarregada] = useFonts({
+  "MontserratRegular": Montserrat_400Regular,
   "MontserratBold": Montserrat_700Bold
- });
+});
 
 if(!fontCarregada){
   return<View />
 }
 
   return (
-    <SafeArray>
+    <SafeAreaView>
       <Cesta />
-      
-    </SafeArray>
+    </SafeAreaView>
   );
 }
